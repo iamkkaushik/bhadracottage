f466b249efe25d77a7f4b346594335a548a0f002
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-16 12:47:35 +0530

    Update index.js

--- src/store/shop/search-slice/index.js
+++ src/store/shop/search-slice/index.js
@@ -10,7 +10,7 @@ export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
     const response = await axios.get(
-      `https://bcserver2.onrender.com/api/shop/search/${keyword}`
+      `https://bcserver3.onrender.com/api/shop/search/${keyword}`
     );
 
     return response.data;
